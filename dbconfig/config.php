944de1fd3504e9403f123a154b66239d71e5b29f
Aashna Naushad <aashnananu@gmail.com>
2020-05-11 20:19:01 +0530

    Update config.php

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,7 +1,7 @@
 <?php
 /*For My LocalPC*/
-$con=mysqli_connect ("remotemysql.com", process.env.DATABASE_NAME, process.env.DATABASE_PASSWORD,process.env.DATABASE_NAME) or die ('I cannot connect to the database because: ' . mysql_error());
-mysqli_select_db ($con,process.env.DATABASE_NAME);
+$con=mysqli_connect ("remotemysql.com", DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME) or die ('I cannot connect to the database because: ' . mysql_error());
+mysqli_select_db ($con,DATABASE_NAME);
 ?>
 
 
