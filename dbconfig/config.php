66cd6a4e472234211d9f3d9ef1516d5a830e4e21
aashnanaushad <aashnananu@gmail.com>
2020-06-15 02:29:27 +0530

    removed comments

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,9 +1,6 @@
 <?php
-
-$name = getenv('DATABASE_NAME');
-$dbpass = getenv('DATABASE_PASSWORD');
-$con=mysqli_connect ("remotemysql.com", $name, $dbpass,$name) or die ('I cannot connect to the database because: ' . mysql_error());
-mysqli_select_db ($con,$name);
+$con=mysqli_connect ("localhost", "root", "","mentoringdb") or die ('I cannot connect to the database because: ' . mysql_error());
+mysqli_select_db ($con,"mentoringdb");
 ?>
 
 
