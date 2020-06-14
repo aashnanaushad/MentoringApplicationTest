043d12b0a6c0b4f7b256335868639b9b07e6a7a2
aashnanaushad <aashnananu@gmail.com>
2020-06-15 02:37:27 +0530

    changed config file

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,6 +1,9 @@
 <?php
-$con=mysqli_connect ("localhost", "root", "","mentoringdb") or die ('I cannot connect to the database because: ' . mysql_error());
-mysqli_select_db ($con,"mentoringdb");
+$dbname=getenv('DATABASE_NAME');
+$dbpass=getenv('DATABASE_PASSWORD');
+$name=getenv('DATABASE_USERNAME')
+$con=mysqli_connect ("remotemysql.com", $name,$dbpass,$dbname) or die ('I cannot connect to the database because: ' . mysql_error());
+mysqli_select_db ($con,$dbname);
 ?>
 
 
