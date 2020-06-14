2be44e9fdfe1e014b07970d1369346bb3e9e8cee
aashnanaushad <aashnananu@gmail.com>
2020-06-15 02:39:20 +0530

    changed config file

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,9 +1,8 @@
 <?php
 $dbname=getenv('DATABASE_NAME');
 $dbpass=getenv('DATABASE_PASSWORD');
-$name=getenv('DATABASE_USERNAME')
-$con=mysqli_connect ("remotemysql.com", $name,$dbpass,$dbname) or die ('I cannot connect to the database because: ' . mysql_error());
-mysqli_select_db ($con,$dbname);
+$con=mysqli_connect ("remotemysql.com", $name,$dbpass,$name) or die ('I cannot connect to the database because: ' . mysql_error());
+mysqli_select_db ($con,$name);
 ?>
 
 
