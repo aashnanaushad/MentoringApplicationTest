79a493a62ccb679c14cd8dfe897d12f9aa901d82
aashnanaushad <aashnananu@gmail.com>
2020-06-15 11:01:14 +0530

    UI modification for hod pages

--- request_fwdhod.php
+++ request_fwdhod.php
@@ -17,16 +17,52 @@
 <html>
 <head>
 <title>hod</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/tailwind.min.css">
 </head>
-<body>
-	<div id="main-wrapper">
+<body class=" bg-blue-400 ">
+	<nav class=" flex items-center justify-between flex-wrap bg-white p-6">
+		<div class="flex items-center flex-shrink-0 text-blue-600 mr-6 ">
+				<span class="font-semibold text-xl tracking-tight"><?php echo $_SESSION['username']; ?>(<?php echo $row1['name'];?>)</span>
+		</div>
+		<div class=" w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+				<div class="text-sm lg:flex-grow">
+				<a href="hod.php" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 mr-4">
+					Profile
+				</a>
+				<a href="request_fwdhod.php" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 mr-4">
+					Requests
+				</a>
+				<a href="assignadvisor.php" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 mr-4">
+					Assign advisor
+				</a>
+				<a href="viewfaculty.php" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 mr-4">
+					List Faculty
+				</a>
+				<a href="slist_dept.php" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 mr-4">
+					List Students
+				</a>
+				<a href="change_pass1.php" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 ">
+					Change Password
+				</a>
+				 </div> 
+				<div>
+				<a href="logout.php" class="inline-block text-sm px-4 py-2 leading-none border rounded text-blue-600 border-blue-600 hover:border-transparent hover:text-blue-600 hover:bg-blue-200 mt-4 lg:mt-0">Logout</a>
+				</div>
+			</div>
+			</nav>
+			<div class=" px-3 py-10 pt-20 bg-blue-400 flex justify-center">
+				<div class="lg:flex bg-white shadow-md rounded px-8 pt-8 pb-10 mb-8 " >
 		<?php
-		    $flag=0;
+			$flag=0;
+			$flag1=0;
 		    $query="select * from request where dept='$dept' and start_yr='$batch'";
-	        $query_run = mysqli_query($con,$query);
+			$query_run = mysqli_query($con,$query);
+			
 		    while($row=$query_run->fetch_assoc() )
 		    {
+				
 		        if($flag==0)
 		        {
 		        @$username= $row["username"];
@@ -35,7 +71,8 @@
 		        {
 		            $username= $row["username"];
 		            $reason=$row["reason"];
-		            $flag=1;
+					$flag=1;
+					$flag=1;
 		            echo"<br>";
 		            echo"<br>";
 		            echo"<br>";
@@ -46,17 +83,20 @@
 		            echo"<br>";
 
 		        }
-		        }
-		    }
+				}
+			}
 		  ?>
+		  		<?php if ($flag1 == 1) { ?>
 		      <form method="post" action="request_fwdhod.php">
 		      <input type="hidden" name=user1 value="<?php echo $username; ?>">
-			  <button name="forward" class="sign_up_btn" type="submit">Approve</button></a>
-			  <a href="hod.php"><button type="button" class="back_btn"><< Back To Home </button></a>
+			  <button name="forward" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline float-left" type="submit">Approve</button></a>
 			    </form>
+				  <?php }
+				  else {
+					echo "NO REQUESTS AVAILABLE";
+				} ?>
 
-		  
-		  <?php
+			<?php 
 			if(isset($_POST['forward']))
 			{
 				$date=date("yy-m-d h:m:s");
@@ -79,7 +119,7 @@
 				}
 			}
 		?>
-		  
+		  </div>
 	</div>
 </body>
 </html>
