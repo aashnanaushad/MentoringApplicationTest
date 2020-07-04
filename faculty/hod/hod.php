ce0414ef0363b9029b28be1b8b6de8d816604bf8
aashnanaushad <aashnananu@gmail.com>
2020-07-05 00:55:58 +0530

    change in page title

--- faculty/hod/hod.php
+++ faculty/hod/hod.php
@@ -16,7 +16,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Faculty</title>
+<title>HOD</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="../../css/style.css">
 <link rel="stylesheet" href="../../css/tailwind.min.css">
--- faculty/home.php
+++ faculty/home.php
@@ -17,7 +17,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Student</title>
+<title>Faculty</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="../css/style.css">
 <link rel="stylesheet" href="../css/tailwind.min.css">
