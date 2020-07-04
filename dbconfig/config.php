437b291e73e27709bf2215675edddc82ab90acf5
aashnanaushad <aashnananu@gmail.com>
2020-07-04 17:10:47 +0530

    updated config.php

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,7 +1,7 @@
 <?php
 $dbname=getenv('DATABASE_NAME');
 $dbpass=getenv('DATABASE_PASSWORD');
-$con=mysqli_connect ("https://remotemysql.com",$dbname,$dbpass,$dbname) or die ('I cannot connect to the database because: ' . mysql_error());
+$con=mysqli_connect ("remotemysql.com",$dbname,$dbpass,$dbname) or die ('I cannot connect to the database because: ' . mysql_error());
 mysqli_select_db ($con,$dbname);
 ?>
 
