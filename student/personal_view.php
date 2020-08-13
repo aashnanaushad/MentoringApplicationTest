88b93effc88aedccc84552f6cd27620fdbedce2c
arafa_naushad <arafanaushad165@gmail.com>
2020-08-13 15:47:58 +0530

    view updated

--- student/personal_view.php
+++ student/personal_view.php
@@ -47,31 +47,44 @@
 
 		<div class=" px-3 py-10 pt-20 bg-blue-400 flex justify-center">
 				<div class="lg:flex bg-white shadow-md rounded px-8 pt-8 pb-10 mb-8 " >
-                 <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
-				
-				</div>
+        
 				<div class="w-full xl:w-3/5 py-6 overflow-y-hidden">
 					
                 <div class=mb-6>
-	            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" >
+	            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"  >
 			       More Details:
 	            </label>
-	                    <p class="text-gray-700 text-base">Family Tree : <br><?php echo $row['famtree'];?></p>
-						<p class="text-gray-700 text-base">Whether you stay along with your parents? <br> <?php echo $row['stay'];?></p>
-                        <p class="text-gray-700 text-base">Is your home life pleasurable ?<br><?php echo $row['life'];?></p>
-						<p class="text-gray-700 text-base">Do you have any social activities ?<br><?php echo $row['social'];?></p>
-						<p class="text-gray-700 text-base">What type of skills you have?<br><?php echo $row['skills'];?></p>
-						<p class="text-gray-700 text-base">What are your hobbies ?<br><?php echo $row['hobby'];?></p>
-                        <p class="text-gray-700 text-base">Which are your thrust areas ?<br><?php echo $row['thrust'];?></p>
-						<p class="text-gray-700 text-base">What is the major objective in your life ?<br><?php echo $row['obj'];?></p>
-						<p class="text-gray-700 text-base">How is your study practice ?<br> <?php echo $row['study'];?></p>
-                        <p class="text-gray-700 text-base">What is your learning pattern ?<br><?php echo $row['learn'];?></p>
-						<p class="text-gray-700 text-base">Do you wish to change your attitude towards studies ?<br><?php echo $row['attitude'];?></p>
-						<p class="text-gray-700 text-base">What are your strengths & weakness in your behaviour ?<br><?php echo $row['behaviour'];?></p>
-						<p class="text-gray-700 text-base">Are you confident in your communication skills ?<br><?php echo $row['communication'];?></p>
-                        <p class="text-gray-700 text-base">Do you have any concerns in general ?<br><?php echo $row['concerns'];?></p>
-						<p class="text-gray-700 text-base">Are you generally a stress free person?<br><?php echo $row['person'];?></p>
-                        <p class="text-gray-700 text-base">Do you have any difficulty with your present theory/practical classes ?<br><?php echo $row['class'];?></p>
+	                    <p class="text-gray-700 text-base"><b>Family Tree : </b> <br><?php echo $row['famtree'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>Whether you stay along with your parents?</b> <br> <?php echo $row['stay'];?></p>
+						<br>
+                        <p class="text-gray-700 text-base"><b>Is your home life pleasurable ?</b><br><?php echo $row['life'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>Do you have any social activities ?</b><br><?php echo $row['social'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>What type of skills you have?</b><br><?php echo $row['skills'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>What are your hobbies ?</b><br><?php echo $row['hobby'];?></p>
+						<br>
+                        <p class="text-gray-700 text-base"><b>Which are your thrust areas ?</b><br><?php echo $row['thrust'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>What is the major objective in your life ?</b><br><?php echo $row['obj'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>How is your study practice ?</b><br> <?php echo $row['study'];?></p>
+						<br>
+                        <p class="text-gray-700 text-base"><b>What is your learning pattern ?</b><br><?php echo $row['learn'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>Do you wish to change your attitude towards studies ?</b><br><?php echo $row['attitude'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>What are your strengths & weakness in your behaviour ?</b><br><?php echo $row['behaviour'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>Are you confident in your communication skills ?</b><br><?php echo $row['communication'];?></p>
+						<br>
+                        <p class="text-gray-700 text-base"><b>Do you have any concerns in general ?</b><br><?php echo $row['concerns'];?></p>
+						<br>
+						<p class="text-gray-700 text-base"><b>Are you generally a stress free person?</b><br><?php echo $row['person'];?></p>
+						<br>
+                        <p class="text-gray-700 text-base" ><b>Do you have any difficulty with your present theory/practical classes ?</b><br><?php echo $row['class'];?></p>
                 </div>
                     
 					<br>
