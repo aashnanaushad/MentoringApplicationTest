11a6b0e2f7d50f3a707e03a53ba24170bb0df98e
aashnanaushad <aashnanaushad@gmail.com>
2020-08-16 22:04:37 +0530

    hod can reject counselor request

--- faculty/hod/request_fwdhod.php
+++ faculty/hod/request_fwdhod.php
@@ -90,6 +90,7 @@
 		      <form method="post" action="request_fwdhod.php">
 		      <input type="hidden" name="user1" value="<?php echo $username; ?>">
 			  <button name="forward" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline float-left" type="submit">Approve</button></a>
+			  <button name="reject" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline float-left" type="submit">Reject</button></a>
 			    </form>
 				  <?php }
 				  else {
@@ -107,6 +108,28 @@
 				{
 					echo '<script type="text/javascript">alert("request approved successfully")</script>';
 					echo "<script>window.location.href='hod.php';</script>";
+				}
+				else
+				{
+					echo '<p class="bg-danger msg-block">Sorry, request could not be granted!</p>';
+				}
+			}
+			if(isset($_POST['reject']))
+			{
+				$username=$_POST['user1'];
+				$query = "delete from request where username='$username'";
+				$query_run = mysqli_query($con,$query);
+				if($query_run)
+				{
+					$query = "update student set rqstcon='0' where username='$username';";
+					$query_run = mysqli_query($con,$query);
+					if($query_run){
+						echo '<script type="text/javascript"> alert("Rejection of request successfull.")</script>';
+						echo "<script>window.location.href='hod.php';</script>";
+					}
+					else {
+						echo '<p class="bg-danger msg-block">Sorry, request could not be granted!</p>';
+					}
 				}
 				else
 				{
