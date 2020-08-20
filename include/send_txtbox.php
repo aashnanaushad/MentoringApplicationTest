e03c5cf003bea276b50f6f3aa5b941c9d289236c
amalreji017 <amalreji017@gmail.com>
2020-08-20 11:17:15 +0530

    notification

--- include/send_txtbox.php
+++ include/send_txtbox.php
@@ -13,13 +13,51 @@
                 $message=$_POST['text'];
                 $date=date("yy-m-d h:m:s");
                 $sql='INSERT INTO message (sender_name,receiver_name,message_text,date_time) VALUES ("'.$sender_name.'","'.$receiver_name.'","'.$message.'","'.$date.'");';
-                 $r=mysqli_query($con,$sql);
+                $r=mysqli_query($con,$sql);
                  if($r){
+                     $sql='SELECT username from reply where username="'.$receiver_name.'";';
+                     $r=mysqli_query($con,$sql);
+                     if($r){
+                         if(mysqli_num_rows($r)==0){
+                             if($flag==1){
+                                 $r1=0;
+                                 $sql='INSERT into reply (`username`,`reply`) values("'.$receiver_name.'","'.$r1.'");';
+                                 $r=mysqli_query($con,$sql);
+                                 if(!$r){
+                                     echo "error!";
+                                 }
+                             }else{
+                                    $r1=1;
+                                    $sql='INSERT into reply (`username`,`reply`) values("'.$sender_name.'","'.$r1.'");';
+                                    $r=mysqli_query($con,$sql);
+                                    if(!$r){
+                                        echo "error!!";
+                                    }
+                                
+                             }
+                         }else{
+                            if($flag==1){
+                                $r1=0;
+                                $sql='UPDATE reply SET reply="'.$r1.'" where username="'.$receiver_name.'";';
+                                $r=mysqli_query($con,$sql);
+                                if(!$r){
+                                    echo "error!";
+                                }
+                            }else{
+                                   $r1=1;
+                                   $sql='UPDATE reply SET reply="'.$r1.'" where username="'.$sender_name.'";';
+                                   $r=mysqli_query($con,$sql);
+                                   if(!$r){
+                                       echo "error!!";
+                                    }
+                                }
+                     } 
                      if($flag==0){
                         echo "<script>window.location.href='right_col.php?user=$receiver_name';</script>";
                      }else{
                         echo "<script>window.location.href='counselorchat.php?user=$receiver_name';</script>";
                      }
+                    }
                  }
                  else{
                     echo "error";
