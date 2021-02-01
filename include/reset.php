b8a2a4eea3a4fb1128105ac38d60a2306228f0c5
amalreji017 <amalreji017@gmail.com>
2021-02-01 20:04:52 +0530

    reset password

--- include/reset.php
+++ include/reset.php
@@ -72,7 +72,7 @@ require '../vendor/autoload.php';
     $mail->addAddress($userEmail);
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Reset your password for the  mentoring app';
-    $mail->Body    = '<p>Sorry to hear you have trouble logging in.<br> Here is your password reset link:<a href='.$url.'>click</a> Here to Reset Password!</p>';
+    $mail->Body    = '<p>Sorry to hear you have trouble logging in.<br> Here is your password reset link:<a href="'.$url.'"><button style="background-color:blue;color:white;width:40px;text-align:center">click<\button></a> Here to Reset Password!</p>';
     $mail->send();
     
     //  $to=$userEmail;
