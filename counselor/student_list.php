1b69457215af68adb0c374f50948269f949595f0
aashnanaushad <aashnanaushad@gmail.com>
2021-02-02 17:31:32 +0530

    UI Changes in student list for counselor

--- counselor/student_list.php
+++ counselor/student_list.php
@@ -45,17 +45,15 @@
 				</div>
 			</div>
 	</nav>
-    <div class=" px-3 py-10 pt-20 bg-blue-400 flex justify-center">
-		<div class="lg:flex bg-white shadow-md rounded px-8 pt-8 pb-10 mb-8 " >
-            <div class="w-full  py-6 overflow-y-hidden"> 
-                <div class="mb-4">
-                    <br>
-                    <h2>Select Department & Batch</h2><br>
+    <div class=" px-2 py-2 bg-blue-400 flex justify-center">
+		<div class="w-3/4 bg-white shadow-md rounded px-4 mb-2 " >
+            <div class="w-full overflow-y-hidden"> 
+                <div class="mb-2">
+                    <h2>Select Department & Batch:</h2></br>
                     <form class="form" action="student_list.php" method="post">
-                        <label class="block text-gray-700 text-md font-bold mb-2" for="department">
+                        <label class="text-gray-700 text-md font-bold mb-2" for="department">
                             Department:
                         </label>
-                        <label ></label>
                         <select id="department" name="department" required>
                             <option value="CSE">CSE</option>
                             <option value="CE">CE</option>
@@ -63,7 +61,7 @@
                             <option value="EEE">EE</option>
                             <option value="ME">ME</option>
                         </select>
-                        <label class="block text-gray-700 text-md font-bold mb-2" for="batch">
+                        <label class=" text-gray-700 text-md font-bold mb-2" for="batch">
                             Batch:
                         </label>
                         <select id="batch" name="batch" required>
@@ -72,16 +70,30 @@
                             <option value="2019">2019</option>
                             <option value="2020">2020</option>
                         </select>
-                        <div class="flex items-center justify-between">
+                        <!-- <div class="flex items-center justify-between"> -->
                             <button class="inline-block text-sm px-4 py-2 leading-none border rounded text-blue-600 border-blue-600 hover:border-transparent hover:text-blue-400 hover:bg-blue-700 mt-4 lg:mt-0" type="submit" name="survey">
                                     Submit
                             </button>
-                        </div>
+                        <!-- </div> -->
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    <div class=" px-1 py-2 bg-blue-400 flex justify-center shadow-md rounded">
+                                <div class="w-3/4 lg:flex bg-white shadow-md rounded px-8 py-4 mb-4 " >
+                                    <div class="w-full  py-2 overflow-y-scroll">
+                                    <table class="text-left w-full ml-8">
+		<thead class="bg-blue-400 flex text-white w-full"> 
+  
+        <tr class="flex w-full mb-2">
+            <th class="p-2 w-1/4">Name</th>
+            <th class="p-2 w-1/4">Department</th>  
+            <th class="p-2 w-1/4">Batch</th>  
+            <th class="p-2 w-1/4">Concern Form</th> 
+        </tr>  
+        </thead>
+        <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style="height: 50vh;">
     <?php
         if(isset($_POST['survey'])){
             $department = $_POST['department'];
@@ -94,9 +106,8 @@
             {
                 if(mysqli_num_rows($query_run)>0){
                     ?>
-                        <div class=" px-3 py-10 pt-20 bg-blue-400 flex justify-center">
-                                <div class="lg:flex bg-white shadow-md rounded px-8 pt-8 pb-10 mb-8 " >
-                                    <div class="w-full  py-6 overflow-y-scroll">
+                        
+
                     <?php                          
                                         //looping through all students
                                         while($row=mysqli_fetch_assoc($query_run)){
@@ -113,21 +124,13 @@
 
                                             //printing 
                                             ?>
-                                                <div class=" w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                                                    <div class="text-sm lg:flex-grow">
-                                                        <a class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 mr-4">
-                                                            <?php echo $name; ?>
-                                                        </a>
-                                                        <a class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 mr-4">
-                                                            <?php echo $department; ?>
-                                                        </a>
-                                                        <a class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-blue-200 mr-4">
-                                                            <?php echo $batch; ?>
-                                                        </a>
+                                                <tr class="flex w-full mb-2">
+                                                        <td class="p-1 w-1/4 overflow-hidden"><?php echo $name; ?></td>
+                                                        <td class="p-1 w-1/4 overflow-hidden"><?php echo $department; ?></td>
+                                                        <td class="p-1 w-1/4 overflow-hidden"><?php echo $batch; ?></td>
                                                     </div> 
-                                                <div>
-                                                    <?php echo'<a href="student_view.php?user='.$name.'" class="inline-block text-sm px-4 py-2 leading-none border rounded text-blue-600 border-blue-600 hover:border-transparent hover:text-blue-400 hover:bg-blue-700 mt-4 lg:mt-0">View</a>'; ?>
-                                                </div>
+                                                    <td class="p-1 w-1/4 overflow-hidden"><?php echo'<a href="student_view.php?user='.$name.'" class="inline-block text-sm px-4 py-1 leading-none border rounded text-blue-600 border-blue-600 hover:border-transparent hover:text-blue-400 hover:bg-blue-700 mt-4 lg:mt-0">View</a>'; ?></td>
+                                                    </tr>  
                                             <?php
                                         }
                     ?>  
